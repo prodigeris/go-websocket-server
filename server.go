d89a5984e638db8be629bcffdd3d28c94ada324a
Arnas Kazlauskas <arnkzl@telesoftas.com>
2019-09-04 02:57:38 +0300

    feat: websocket endpoint

--- server.go
+++ server.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
 	"net/http"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 type WebSocketServer struct {
 	http.Handler
@@ -13,11 +19,16 @@ func (s WebSocketServer) homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hi!")
 }
 
+func (s WebSocketServer) webSocket(w http.ResponseWriter, r *http.Request) {
+	upgrader.Upgrade(w, r, nil)
+}
+
 func NewServer() *WebSocketServer {
 	server := new(WebSocketServer)
 
 	router := http.NewServeMux()
 	router.Handle("/", http.HandlerFunc(server.homePage))
+	router.Handle("/ws", http.HandlerFunc(server.webSocket))
 
 	server.Handler = router
 
--- server_test.go
+++ server_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -15,5 +17,20 @@ func TestServer(t *testing.T) {
 		NewServer().ServeHTTP(response, request)
 
 		assert.Equal(t, "Hi!", response.Body.String())
+	})
+
+	t.Run("Should open websockets on ws endpoint", func(t *testing.T) {
+
+		server := httptest.NewServer(NewServer())
+		defer server.Close()
+
+		wsURL := "ws" + strings.TrimPrefix(server.URL, "http") + "/ws"
+
+		ws, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
+		if err == nil {
+			defer ws.Close()
+		}
+
+		assert.NoError(t, err)
 	})
 }
