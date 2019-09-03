ea6457f2e9187bb3a4df68b10950849dc30743ff
Arnas Kazlauskas <arnkzl@telesoftas.com>
2019-09-03 20:40:58 +0300

    feat: introduce server with homepage

---
+++ server_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	t.Run("Should return homepage", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		WebSocketServer(response, request)
+
+		got := response.Body.String()
+		want := "Hi!"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
